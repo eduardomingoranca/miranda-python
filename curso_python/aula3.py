23a0a158504686e215e638be863c9b94ecd33753
eduardomingoranca <eduardomingoranca@gmail.com>
2024-01-27 15:58:10 -0300

    tipo str(string) - introducao aos tipos de dados

---
+++ curso_python/aula3.py
@@ -0,0 +1,22 @@
+"""
+Python = Linguagem de programacao
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings sao textos que estao dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Louis Octavius')
+print(1, 'Louis "Octavius"')
+
+# Aspas duplas
+print("Louis Octavius")
+print(2, "Louis 'Octavius'")
+
+
+# Escape
+print('Louis \'Octavius\'')
+
+# r
+print(r'Louis \'Octavius\'')
