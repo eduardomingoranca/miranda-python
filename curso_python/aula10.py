250141a2103d1920a3234317dd55e2ecb1981f45
eduardomingoranca <eduardomingoranca@gmail.com>
2024-01-28 14:59:41 -0300

    concatenacao (+) e repeticao (*) com operadores aritmeticos

---
+++ curso_python/aula10.py
@@ -0,0 +1,8 @@
+#concatenacao = 'A' + 'B' + 'C'
+concatenacao = 'Louis' + ' ' + 'Octavius'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_louis = 3 * 'Louis'
+print(a_dez_vezes)
+print(tres_vezes_louis)
