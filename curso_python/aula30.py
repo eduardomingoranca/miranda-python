39689e2d875fe13c295723eecf8368d42109f635
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-01 19:58:30 -0300

    parte 1: variaveis, constantes e complexidade de codigo

---
+++ curso_python/aula30.py
@@ -0,0 +1,11 @@
+"""
+CONSTANTE = 'Variaveis' que nao vao mudar
+Muitas condicoes no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+"""
+velocidade = 61 # velocidade atual do carro
+local_carro = 101 # local em que o carro esta na estrada
+
+RADAR_1 = 60 # velocidade maxima do radar 1
+LOCAL_1 = 100 # local onde o radar 1 esta
+RADAR_RANGE = 1 # A distancia onde o radar pega
