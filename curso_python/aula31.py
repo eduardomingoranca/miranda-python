2da4379a5d1825ec1b850acc36022aa65a25e729
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-03 13:41:31 -0300

    id - a identidade do valor que esta na memoria

---
+++ curso_python/aula31.py
@@ -0,0 +1,11 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Nao valor
+is e is not = eh ou nao eh (tipo, valor, identidade)
+id = Identidade
+"""
+
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
