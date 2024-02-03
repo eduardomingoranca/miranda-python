60b7c2001339b6c6aa19756945170b422f42283f
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-03 13:52:16 -0300

    flags, is, is not e none

--- curso_python/aula31.py
+++ curso_python/aula31.py
@@ -5,7 +5,24 @@ is e is not = eh ou nao eh (tipo, valor, identidade)
 id = Identidade
 """
 
-v1 = 'a'
-v2 = 'b'
-print(id(v1))
-print(id(v2))
+#v1 = 'a'
+#v2 = 'b'
+#print(id(v1))
+#print(id(v2))
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    passou_no_if = True
+    print('Faca algo')
+else:
+    print('Nao faca algo')
+
+#print(passou_no_if, passou_no_if is None)
+#print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Nao passou no if')
+else:
+    print('Passou no if')
