930ca03c580079dca975f97d4d427d30ef05cbf0
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-03 13:34:44 -0300

    parte 2: variaveis, constantes e complexidade de codigo

--- curso_python/aula30.py
+++ curso_python/aula30.py
@@ -9,3 +9,16 @@ local_carro = 101 # local em que o carro esta na estrada
 RADAR_1 = 60 # velocidade maxima do radar 1
 LOCAL_1 = 100 # local onde o radar 1 esta
 RADAR_RANGE = 1 # A distancia onde o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
+
+if vel_carro_pass_radar_1:
+    print('Velocidade carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar_1:
+    print('Carro multado em radar 1')
