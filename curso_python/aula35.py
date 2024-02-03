314e8f9c2eb3988f89a354020aaeb376af990f22
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-03 15:08:14 -0300

    while - condicao em detalhes

---
+++ curso_python/aula35.py
@@ -0,0 +1,13 @@
+"""
+Repeticoes
+while (enquanto)
+Executa uma acao enquanto uma condicao for verdadeira
+Loop infinito -> Quando um codigo nao tem fim
+"""
+contador = 0
+
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+
+print('Acabou')
