adcf39777612977e2bdc419a4a963f77578cfb51
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-03 15:32:10 -0300

    operadores de atribuicao com operadores aritmeticos

---
+++ curso_python/aula36.py
@@ -0,0 +1,39 @@
+"""
+Operadores de atribuicao
+= += -= *= /= //= **= %=
+"""
+#contador = 0
+#contador = '1'
+contador = 10
+#contador = 2
+
+###
+
+#contador += 2
+#print(contador)
+
+###
+
+#contador += '2'
+#print(contador)
+
+###
+
+#contador *= '2'
+#print(contador)
+
+###
+
+#contador %= 2
+#print(contador)
+
+###
+
+#contador **= 10
+#print(contador)
+
+###
+
+#contador //= 5
+contador /= 5
+print(contador)
