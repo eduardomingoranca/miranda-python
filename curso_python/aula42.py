1aded79813d61aa9c601ac5bbf3f81e610ccb84d
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-11 15:47:43 -0300

    debugger: while - qual letra apareceu mais vezes na frase?

--- curso_python/aula42.py
+++ curso_python/aula42.py
@@ -10,7 +10,8 @@ frase = 'O Python eh uma linguagem de programacao ' \
 
 i = 0
 qtd_apareceu_mais_vezes = 0
-letra_que_apareceu_mais_vezes = ''
+letra_apareceu_mais_vezes = ''
+
 while i < len(frase):
     letra_atual = frase[i]
 
@@ -18,16 +19,16 @@ while i < len(frase):
         i += 1
         continue
 
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+    qtd_atual = frase.count(letra_atual)
 
-    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
-        letra_que_apareceu_mais_vezes = letra_atual
+    if qtd_apareceu_mais_vezes <= qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
 
     i += 1
 
 print(
     'A letra que apareceu mais vezes foi '
-    f'"{letra_que_apareceu_mais_vezes}" que apareceu '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
     f'{qtd_apareceu_mais_vezes}x'
 )
