30e0f3fbef8901d9c8ffd716b0400e1e66b1c575
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-12 14:06:50 -0300

    inserindo itens em qualquer indice da lista com insert (tipo list)

--- curso_python/aula48.py
+++ curso_python/aula48.py
@@ -3,7 +3,14 @@ Listas em Python
 Tipo list - Mutavel
 Suporta varios valores de qualquer tipo
 Conhecimentos reutilizaveis - indices e fatiamento
-Metodos uteis: append, insert, pop, del, clear, extend, +
+Metodos uteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no indice escolhido
+    pop    - Remove do final ou do indice escolhido
+    del    - Apaga um indice
+    clear  - Limpa a lista
+    extend - Estende a lista
+    + -      Concatena listas
 Create Read Update   Delete
 Criar, Ler, Alterar, Apagar = lista[i] (CRUD)
 """
@@ -21,15 +28,23 @@ lista[-3] = 'Mary'
 #print(lista[2], type(lista[2]))
 
 lista = [10, 20, 30, 40]
+lista.append('Louis')
+nome = lista.pop()
+lista.append(1233)
+del lista[-1]
+# lista.clear()
+lista.insert(0, 5)
+print(lista)
+
 #lista[2] = 300
 #del lista[2]
 #print(lista)
 #print(lista[2])    
-lista.append(50)
-lista.pop()
-lista.append(60)
-ultimo_valor = lista.pop(3)
-print(lista, 'Removido,', ultimo_valor)
+#lista.append(50)
+#lista.pop()
+#lista.append(60)
+#ultimo_valor = lista.pop(3)
+#print(lista, 'Removido,', ultimo_valor)
 
 #numero = lista[2]
 #print(numero)
