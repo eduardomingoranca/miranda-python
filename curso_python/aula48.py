5e4fc6b72727ea207ec84b8e1c1e6e7955cb9d9d
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-12 14:12:11 -0300

    concatenando e estendendo listas em python

--- curso_python/aula48.py
+++ curso_python/aula48.py
@@ -34,7 +34,7 @@ lista.append(1233)
 del lista[-1]
 # lista.clear()
 lista.insert(0, 5)
-print(lista)
+#print(lista)
 
 #lista[2] = 300
 #del lista[2]
@@ -48,3 +48,9 @@ print(lista)
 
 #numero = lista[2]
 #print(numero)
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
