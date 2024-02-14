3314eec955fca576b21a7ea4109fbbd946b21920
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-14 19:47:29 -0300

    detalhes sobre o interpretador do python

---
+++ curso_python/aula58.py
@@ -0,0 +1,31 @@
+"""
+Interpretador do Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (interativo com mod)
+
+The Zen of Python, por Tim Peters
+
+Bonito eh melhor que feio.
+Explicito eh melhor que implicito.
+Simples eh melhor que complexo.
+Complexo eh melhor que complicado.
+Plano eh melhor que aglomerado.
+Esparso eh melhor que denso.
+Legibilidade conta.
+Casos especiais nao sao especiais o bastante para quebrar as regras.
+Embora a praticidade venca a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentacao de adivinhar.
+Deve haver um -- e so um -- modo obvio para fazer algo.
+Embora esse modo possa nao ser obvio a primeira vista a menos que voce seja holandes.
+Agora eh melhor que nunca.
+Embora nunca frequentemente seja melhor que *exatamente* agora.
+Se a implementacao eh dificil de explicar, eh uma ma ideia.
+Se a implementacao eh facil de explicar, pode ser uma boa ideia.
+Namespaces sao uma grande ideia -- vamos fazer mais dessas! 
+"""
