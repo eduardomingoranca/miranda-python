00539962ac2dd89a74144267be4e9c5903f1d211
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-25 10:22:48 -0300

    exercicio com funcoes

---
+++ curso_python/aula75.py
@@ -0,0 +1,21 @@
+# Exercicios
+# Crie funcoes que duplicam, triplicam e quadruplicam
+# o numero recebido como parametro.
+
+
+def duplicar(numero):
+    return numero * 2
+
+
+def triplicar(numero):
+    return numero * 3
+
+
+def quadruplicar(numero):
+    return numero * 4
+
+
+print('duplicar:', duplicar(3))
+print('triplicar:', triplicar(4))
+print('quadruplicar:', quadruplicar(5))
+
