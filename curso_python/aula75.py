cd053d20fcd0d9c51c0493898b2df5ed5fe7e6af
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-25 10:26:14 -0300

    solucao do exercicio com funcoes

--- curso_python/aula75.py
+++ curso_python/aula75.py
@@ -3,19 +3,34 @@
 # o numero recebido como parametro.
 
 
-def duplicar(numero):
-    return numero * 2
+#def duplicar(numero):
+#    return numero * 2
 
 
-def triplicar(numero):
-    return numero * 3
+#def triplicar(numero):
+#    return numero * 3
 
 
-def quadruplicar(numero):
-    return numero * 4
+#def quadruplicar(numero):
+#    return numero * 4
 
 
-print('duplicar:', duplicar(3))
-print('triplicar:', triplicar(4))
-print('quadruplicar:', quadruplicar(5))
+#print('duplicar:', duplicar(3))
+#print('triplicar:', triplicar(4))
+#print('quadruplicar:', quadruplicar(5))
+
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(3)
+triplicar = criar_multiplicador(4)
+quadruplicar = criar_multiplicador(5)
+
+print('duplicar:', duplicar(2))
+print('triplicar:', triplicar(3))
+print('quadruplicar:', quadruplicar(4))
 
