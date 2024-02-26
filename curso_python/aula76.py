54cabb80fac9c9c6b620d4caeec20089f2f0f06b
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-26 14:21:39 -0300

    (parte 2) metodos uteis nos dicionarios python (dict)

--- curso_python/aula76.py
+++ curso_python/aula76.py
@@ -33,11 +33,48 @@ pessoa = {
     ]
 } 
 #print(pessoa, type(pessoa))
-print(pessoa['nome'])
-print(pessoa['sobrenome'])
+#print(pessoa['nome'])
+#print(pessoa['sobrenome'])
 
-print()
+#print()
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+#for chave in pessoa:
+#    print(chave, pessoa[chave])
 
+
+# Metodos uteis dos dicionarios em Python
+# len - quantas chaves
+# keys - iteravel com as chaves
+# values - iteravel com os valores
+# items - iteravel com chaves e valores
+# setdefault - adiciona valor se a chave nao existe
+# copy - retorna uma copia rasa (shallow copy)
+# get - obtem uma chave
+# pop - apaga um item com a chave especificada (del)
+# popitem - apaga o ultimo item adicionado
+# update - atualiza um dicionario com outro
+p1 = {
+    'nome': 'Enoch',
+    'sobrenome': 'ben Kohen'
+}
+#print(p1['nome'])
+#print(p1.get('nome', 'Nao existe'))
+
+#nome = p1.pop('nome')
+#print(nome)
+#print(p1)
+
+#ultima_chave = p1.popitem()
+#print(ultima_chave)
+#print(p1)
+
+#p1.update({
+#    'nome': 'novo valor',
+#    'idade': 30
+#})
+#print(p1)
+#p1.update(nome='novo valor', idade=30)
+#tupla = (('nome', 'novo valor'), ('idade', 30))
+lista = [['nome', 'novo valor'], ['idade', 30]]
+p1.update(lista)
+print(p1)
