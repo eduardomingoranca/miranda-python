dbe46b8afffcf6a81d2eb09bc6695c6a70f95bb7
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 19:40:22 -0300

    na pratica: polimorfismo, assinatura de metodos e liskov substitution principle

--- curso_python/aula144.py
+++ curso_python/aula144.py
@@ -11,3 +11,42 @@
 # Principio da substituicao de liskov
 # Objetos de uma superclasse devem ser substituiveis
 # por objetos de uma subclasse sem quebrar a aplicacao.
+# Sobrecarga de metodos (overload)  🐍 = ❌
+# Sobreposicao de metodos (override) 🐍 = ✅
+from abc import ABC, abstractmethod
+
+
+class Notificacao(ABC):
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('Email: enviando - ', self.mensagem)
+        return True
+
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando - ', self.mensagem)
+        return False
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificacao enviada')
+    else:
+        print('Notificacao Nao enviada')
+
+
+notificacao_email = NotificacaoEmail('testando email')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('testando SMS')
+notificar(notificacao_sms)
