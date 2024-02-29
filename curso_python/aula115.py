80b76ca06e5a7f3add72a30ae6dcf66481f471b3
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 16:43:45 -0300

    como criar o seu ambiente virtual com venv

--- curso_python/aula115.py
+++ curso_python/aula115.py
@@ -8,3 +8,8 @@
 # Voce pode dar o nome que preferir para um
 # ambiente virtual, mas os mais comuns sao:
 # venv env .venv .env
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
