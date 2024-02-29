0ba5db5f14b640cda25299b8f7812fc17d95c1ba
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 19:37:45 -0300

    teoria: polimorfismo, assinatura de metodos e liskov substitution principle

---
+++ curso_python/aula144.py
@@ -0,0 +1,13 @@
+# Polimorfismo em Python Orientado a Objetos
+# Polimorfismo eh o principio que permite que
+# classes deridavas de uma mesma superclasse
+# tenham metodos iguais (com mesma assinatura)
+# mas comportamentos diferentes.
+# Assinatura do metodo = Mesmo nome e quantidade
+# de parametros (retorno nao faz parte da assinatura)
+# Opiniao + principios que contam:
+# Assinatura do metodo: nome, parametros e retorno iguais
+# SO"L"ID
+# Principio da substituicao de liskov
+# Objetos de uma superclasse devem ser substituiveis
+# por objetos de uma subclasse sem quebrar a aplicacao.
