3d1c7fc2ac9656646fc19f27ab679743335c24a1
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 16:56:48 -0300

    modos de abertura de arquivo e encoding com with open

--- curso_python/aula116.py
+++ curso_python/aula116.py
@@ -1,4 +1,5 @@
-# with open (context manager) e métodos úteis do TextIOWrapper
+# Leia tambem: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
+# with open (context manager) e metodos uteis do TextIOWrapper
 # Usamos a funcao open para abrir
 # um arquivo em Python (ele pode ou nao existir)
 # Modos:
@@ -18,33 +19,39 @@
 # Vamos falar mais sobre o modulo json, mas:
 # json.dump = Gera um arquivo json
 # json.load
-caminho_arquivo = 'aula116.txt'
+caminho_arquivo = 'docs/aula116.txt'
 
 # arquivo = open(caminho_arquivo, 'w')
 # #
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atencao\n')
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
-    arquivo.writelines(
-        ('Linha 3\n', 'Linha 4\n')
-    )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
-
-    print('READLINES')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
-
-
-print('#' * 10)
-
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
+    arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
