8d0e9f4d9725c0f04495b606f77bbab7930f74f4
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 18:51:13 -0300

    teoria: heranca, generalizacao e especializacao

---
+++ curso_python/aula138.py
@@ -0,0 +1,10 @@
+# Heranca simples - Relacoes entre classes
+# Associacao - usa, Agregacao - tem
+# Composicao - Eh dono de, Heranca - Eh um
+#
+# Heranca vs Composicao
+#
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
