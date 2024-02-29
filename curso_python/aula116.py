5a571820648832b43fa70539ba2991e9400cba8a
eduardomingoranca <eduardomingoranca@gmail.com>
2024-02-29 16:59:47 -0300

    os.remove, os.unlink e os.rename - apagando, renomeando e movendo arquivos

--- curso_python/aula116.py
+++ curso_python/aula116.py
@@ -1,3 +1,5 @@
+import os
+
 # Leia tambem: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
 # with open (context manager) e metodos uteis do TextIOWrapper
 # Usamos a funcao open para abrir
@@ -54,4 +56,9 @@ with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
     arquivo.write('Atencao\n')
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
-    arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+
+# os.remove(caminho_arquivo) # ou unlink
+# os.rename(caminho_arquivo, 'aula116-2.txt')
