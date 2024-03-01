82e89cf8c0f63e4d468f383391555487b0677108
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-01 12:41:32 -0300

    dataclasses com metodos property e setter

--- curso_python/aula159.py
+++ curso_python/aula159.py
@@ -11,10 +11,20 @@ from dataclasses import dataclass
 @dataclass
 class Pessoa:
     nome: str
-    idade: int
+    sobrenome: str
 
+    @property
+    def nome_completo(self):
+        return f'{self.nome} {self.sobrenome}'
+
+    @nome_completo.setter
+    def nome_completo(self, valor):
+        nome, *sobrenome = valor.split()
+        self.nome = nome
+        self.sobrenome = ' '.join(sobrenome)
 
 if __name__ == '__main__':
-    p1 = Pessoa('Louis', 30)
-    p2 = Pessoa('Louis', 30)
-    print(p1 == p2)
+    p1 = Pessoa('Louis', 'Octavius')
+    p1.nome_completo = 'Mary Helen'
+    print(p1)
+    print(p1.nome_completo)
