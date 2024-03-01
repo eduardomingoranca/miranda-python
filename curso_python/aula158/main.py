18c4af09711a83e1d3a8ba904107f70aa2ada970
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-01 11:09:20 -0300

    criando a classe banco

--- curso_python/aula158/main.py
+++ curso_python/aula158/main.py
@@ -28,11 +28,29 @@ Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
     polimorfismo - as subclasses que implementam o metodo sacar)
 Criar classe Banco para AGREGAR classes de clientes e de contas (Agregacao)
 Banco sera responsavel autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregacao)
+    Banco tem contas e clientes (Agregacao)
     * Checar se a agencia eh daquele banco
     * Checar se o cliente eh daquele banco
     * Checar se a conta eh daquele banco
 So sera possivel sacar se passar na autenticacao do banco (descrita acima)
-Banco autentica por um metodo.
+Banco autentica por um metodo (autenticar).
 """
 import contas
+import pessoas
+from banco import Banco
+
+c1 = pessoas.Cliente('Louis', 30)
+cc1 = contas.ContaCorrente(111, 222, 0, 0)
+c1.conta = cc1
+c2 = pessoas.Cliente('Mary', 18)
+cp1 = contas.ContaPoupanca(112, 223, 100)
+c2.conta = cp1
+banco = Banco()
+banco.clientes.extend([c1, c2])
+banco.contas.extend([cc1, cp1])
+banco.agencias.extend([111, 222])
+
+if banco.autenticar(c1, cc1):
+    cc1.depositar(10)
+    c1.conta.depositar(100)
+    print(c1.conta)
