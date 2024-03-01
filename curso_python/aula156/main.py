55506337e38542f1be00d3cb1f7e6456a20f12fd
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-01 10:31:27 -0300

    docstring em class (documentacao)

--- curso_python/aula156/main.py
+++ curso_python/aula156/main.py
@@ -1,6 +1,7 @@
 # import uma_linha
-#import varias_linha
-import documentando_funcoes
+# import varias_linha
+# import documentando_funcoes
+import documentando_class
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
@@ -8,4 +9,5 @@ import documentando_funcoes
 # print(uma_linha.__name__)
 #help(uma_linha)
 #help(varias_linha)
-help(documentando_funcoes)
+#help(documentando_funcoes)
+help(documentando_class)
