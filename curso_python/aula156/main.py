cb4961f77b2bc5b1149126972180a8cf808241a9
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-01 10:25:54 -0300

    docstring em funcoes (documentacao)

--- curso_python/aula156/main.py
+++ curso_python/aula156/main.py
@@ -1,9 +1,11 @@
 # import uma_linha
-import varias_linha
+#import varias_linha
+import documentando_funcoes
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 #help(uma_linha)
-help(varias_linha)
+#help(varias_linha)
+help(documentando_funcoes)
