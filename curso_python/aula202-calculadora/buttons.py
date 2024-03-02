8577ac3e199721cef23aaa4443fca3e6e17e8ab4
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-02 13:44:47 -0300

    calculadora: preenchendo a grid de botoes

--- curso_python/aula202-calculadora/buttons.py
+++ curso_python/aula202-calculadora/buttons.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QGridLayout, QPushButton
+from utils import isEmpty, isNumOrDot
 from variables import MEDIUM_FONT_SIZE
 
 
@@ -19,10 +20,21 @@ class ButtonsGrid(QGridLayout):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self._grid_mask = [
+        self._gridMask = [
             ['C', '◀', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
+        self._makeGrid()
+
+    def _makeGrid(self):
+        for rowNumber, rowData in enumerate(self._gridMask):
+            for colNumber, buttonText in enumerate(rowData):
+                button = Button(buttonText)
+
+                if not isNumOrDot(buttonText) and not isEmpty(buttonText):
+                    button.setProperty('cssClass', 'specialButton')
+
+                self.addWidget(button, rowNumber, colNumber)
