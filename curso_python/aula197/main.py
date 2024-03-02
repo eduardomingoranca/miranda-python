9981d8617fa3a16a7006e6d4441b95eeb4d569d7
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-02 10:41:58 -0300

    pyPDF2 para manipular arquivos PDF (instalacao)

---
+++ curso_python/aula197/main.py
@@ -0,0 +1,9 @@
+# # PyPDF2 para manipular arquivos PDF (Instalacao)
+# PyPDF2 eh uma biblioteca de manipulacao de arquivos PDF feita em Python puro,
+# gratuita e de codigo aberto. Ela eh capaz de ler, manipular, escrever e unir
+# dados de arquivos PDF, assim como adicionar anotacoes, transformar paginas,
+# extrair texto e imagens, manipular metadados, e mais.
+# A documentacao contem todas as informacoes necessarias para usar PyPDF2.
+# Link: https://pypdf2.readthedocs.io/en/3.0.0/
+# Ative seu ambiente virtual
+# pip install pypdf2
