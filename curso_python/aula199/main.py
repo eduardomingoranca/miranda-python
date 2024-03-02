d801927557539aea29b4cf98e06672cb41d5c7f1
eduardomingoranca <eduardomingoranca@gmail.com>
2024-03-02 11:01:22 -0300

    openpyxl para arquivos excel xlsx, xlsm, xltx e xltm (instalacao)

---
+++ curso_python/aula199/main.py
@@ -0,0 +1,9 @@
+# openpyxl para arquivos Excel xlsx, xlsm, xltx e xltm (instalacao)
+# Com essa biblioteca sera possivel ler e escrever dados em celulas
+# especificas, formatar celulas, inserir graficos,
+# criar formulas, adicionar imagens e outros elementos graficos as suas
+# planilhas. Ela eh util para automatizar tarefas envolvendo planilhas do
+# Excel, como a criacao de relatorios e analise de dados e/ou facilitando a
+# manipulacao de grandes quantidades de informacoes.
+# Instalacao necessaria: pip install openpyxl
+# Documentacao: https://openpyxl.readthedocs.io/en/stable/
